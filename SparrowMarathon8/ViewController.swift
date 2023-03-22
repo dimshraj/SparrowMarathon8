a813a11f6c1107a6d7ac1a023ba44474f391b1b5
shraiber <shraiber@ujin.tech>
2023-03-22 21:47:08 +0200

    first iteration

--- SparrowMarathon8/ViewController.swift
+++ SparrowMarathon8/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.delegate = self
+//        scrollView.contentInsetAdjustmentBehavior = .never
+        return scrollView
+    }()
+    private let imageView = UIImageView(image: UIImage(systemName: "person.crop.circle.fill" ))
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        view.backgroundColor = .systemBackground
+        self.title = "Avatar"
+        setupNavigationBar()
+        setupScrollView()
+        self.view.backgroundColor = .white
+    }
+    
+    private func setupNavigationBar() {
+
+        guard let navigationBar = self.navigationController?.navigationBar else { return }
+        navigationBar.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.rightAnchor.constraint(equalTo: navigationBar.rightAnchor,
+                                             constant: -16),
+            imageView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor,
+                                              constant: -5),
+            imageView.heightAnchor.constraint(equalToConstant: 40),
+            imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor)
+        ])
     }
 
-
+    private func setupScrollView() {
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.scrollView.contentSize = .init(width: self.view.frame.width, height: 2000)
+       
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(navigationController?.navigationBar.frame.size)
+       
+    }
 }
-

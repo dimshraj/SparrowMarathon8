da221345dbb258f526dd0bf2d20dd205603edb34
shraiber <shraiber@ujin.tech>
2023-03-22 21:37:38 +0200

    Initial Commit

---
+++ SparrowMarathon8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparrowMarathon8
+//
+//  Created by Dmitriy Shrayber on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
